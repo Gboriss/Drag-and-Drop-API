e047fc5ad1f938aa29ba41c5822f97ead8ad2927
Gboriss <galinaborissenko@gmail.com>
2020-02-01 19:53:12 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const initianalData = {
+    column: { 
+        id: 'column-1',
+        numberIds: ['four', 'one', 'five', 'three', 'two']
+    },
+    numbers: {
+        'four': {id: 'four', content: '4'},
+        'one': {id: 'four', content: '1'},
+        'five': {id: 'five', content: '5'},
+        'three': {id: 'three', content: '3'},
+        'two': {id: 'two', content: '2'},
+
+    }
+}
---
+++ render.js
@@ -0,0 +1,6 @@
+class NumbersGame extends React.Component< any, INumbersGameState > {
+    
+    public constructor(props: any) {
+        super(props)
+    }
+}
